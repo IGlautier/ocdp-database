f21112967a432824111802de7ec695dc7ea29882
Ubuntu <ubuntu@ip-172-30-1-138.eu-west-1.compute.internal>
2015-07-07 14:37:29 +0000

    start of couchdb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var cradle = require('cradle');
+
+var con = new(cradle.Connection)('http://node:dbp4sS123@localhost', 5984, {
+      cache: true,
+      raw: false,
+      forceSave: true
+  });
+
+var devices = con.database('devices');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	devices.all(function(err, res) {
+		if (err) {
+			console.log('Error: %s', err)
+		} else {
+			console.log(res);
+		}
+	});
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
