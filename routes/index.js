f96b0235e2ee6fca5a1f31778c3b388a957c8316
IGlautier <ivanglautier@gmail.com>
2015-07-08 16:06:21 +0100

    Database submission

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var cradle = require('cradle');
 var util = require('util');
 
+
 var con = new(cradle.Connection)('http://localhost', 5984, {
       cache: true,
       raw: false,
@@ -23,12 +24,26 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/newDevice', function(req, res, next) {
+	if(req.query.success) var added = true;
 
-	res.render('add', { title: 'OCDP Device Database' });
+	if(req.query.failure) var added = false;
+
+	console.log(req.query);
+	res.render('add', { title: 'OCDP Device Database', success: added});
 
 });
 
 router.post('/newDevice', function(req, res, next) {
+	
+	var timestamp = (new Date).getTime();
+	devices.save({
+		name: req.body.deviceName, type: req.body.type, manufacturer : req.body.manufacturer, website: req.body.website, git: req.body.git, description: req.body.description, dateAdded: timestamp
+	}, function (err, dbRes) {
+		console.log(dbRes);
+		if(err) res.redirect('/newDevice?failure=true');
+		else res.redirect('/newDevice?success=true');
+	});
+	
 });
 
 module.exports = router;
