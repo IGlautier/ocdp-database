8fb7b9d5bd6ca315ce2f7e85576b6ac966106a79
IGlautier <ivanglautier@gmail.com>
2015-07-08 11:21:37 +0100

    Home page and database functionality
    Added initial work for adding devices as well as fixing device pulling
    from database

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var cradle = require('cradle');
+var util = require('util');
 
-var con = new(cradle.Connection)('http://node:dbp4sS123@localhost', 5984, {
+var con = new(cradle.Connection)('http://localhost', 5984, {
       cache: true,
       raw: false,
       forceSave: true
   });
 
-var devices = con.database('devices');
+var devices = con.database('device');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	devices.all(function(err, res) {
-		if (err) {
-			console.log('Error: %s', err)
-		} else {
-			console.log(res);
-		}
+	devices.view('devicelist/all', function (err, data) {
+		var nDev = [];
+		for(var i = data.length-1; i > data.length-4; i--) nDev.push(data[i]);
+		console.log(util.inspect(nDev));
+		res.render('index', { title: 'OCDP Device Database', newDevices: nDev });
 	});
-	res.render('index', { title: 'Express' });
+	
+});
+
+router.get('/newDevice', function(req, res, next) {
+
+	res.render('add', { title: 'OCDP Device Database' });
+
+});
+
+router.post('/newDevice', function(req, res, next) {
 });
 
 module.exports = router;
